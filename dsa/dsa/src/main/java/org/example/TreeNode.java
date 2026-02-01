0b1dab3eff255bf5512f658f1b8f7cecff5cb0c2
nazmul <nazmulcuet11@gmail.com>
2026-02-01 16:26:36 +0700

    tree basic

---
+++ dsa/dsa/src/main/java/org/example/TreeNode.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class TreeNode {
+    int  val;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
